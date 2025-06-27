ff85068b18034677c727553a90ceec4c0dc481b1
iamaniket58 <iamaniket58@gmail.com>
2025-06-27 19:13:25 +0530

    InitialCode

---
+++ App.js
@@ -0,0 +1,7 @@
+//const x = React.createElement("div", { "id": "parent" }, React.createElement("div", { "id": "child" }, [React.createElement("h1", {"id":"heeader1"}, "This is my h1 header"),React.createElement("h2",{"id":"heeader2"},"This is h2 header")]))
+const y = React.createElement("div", { "id": "parent", "class":"myclass" }, React.createElement("div", { "id": "child" }, React.createElement("h1", {"id":"heeader1"}, "This is my h1 header"),React.createElement("h2",{"id":"heeader2"},"This is h2 header")))
+
+
+const heading = React.createElement("h1", { "id": "myId" }, "Hello from React!!")
+const root = ReactDOM.createRoot(document.getElementById("roots"));
+root.render(y);
