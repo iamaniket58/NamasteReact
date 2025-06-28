7cb94130333d486c8d7de77d0482d6166ade4180
iamaniket58 <iamaniket58@gmail.com>
2025-06-28 21:18:38 +0530

    Ignited our app

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
 //const x = React.createElement("div", { "id": "parent" }, React.createElement("div", { "id": "child" }, [React.createElement("h1", {"id":"heeader1"}, "This is my h1 header"),React.createElement("h2",{"id":"heeader2"},"This is h2 header")]))
-const y = React.createElement("div", { "id": "parent", "class":"myclass" }, React.createElement("div", { "id": "child" }, React.createElement("h1", {"id":"heeader1"}, "This is my h1 header"),React.createElement("h2",{"id":"heeader2"},"This is h2 header")))
+const y = React.createElement("div", { "id": "parent" }, React.createElement("div", { "id": "child" }, React.createElement("h1", {"id":"heeader1"}, "This is my h1 header"),React.createElement("h2",{"id":"heeader2"},"This is Awesome header")))
 
 
-const heading = React.createElement("h1", { "id": "myId" }, "Hello from React!!")
+const heading = React.createElement("h2", { "id": "myId" }, "Hello from React!!")
 const root = ReactDOM.createRoot(document.getElementById("roots"));
 root.render(y);
--- Readme.md
+++ Readme.md
@@ -1,1 +1,31 @@
 #Namaste React
+
+Here are some git commands
+# 1. Initialize a new Git repository
+git init
+
+# 2. Add files to staging
+git add App.js
+git add index.html
+git add index1.html
+
+# 3. Commit the changes with a message
+git commit -m "InitialCode"
+
+# 4. Rename the default branch to 'main'
+git branch -M main
+
+# 5. Add the remote repository (SSH URL)
+git remote add origin git@github.com:iamaniket58/NamasteReact.git
+
+# 6. Generate SSH key (only run once if not generated)
+ssh-keygen -t ed25519 -C "iamaniket58@gmail.com"
+
+# 7. Copy SSH public key to clipboard (paste it on GitHub → SSH keys)
+clip < ~/.ssh/id_ed25519.pub
+
+# 8. Test SSH connection (optional but recommended)
+ssh -T git@github.com       
+
+# 9. Push the code to GitHub
+git push -u origin main
