831eea60e7dc6f274b5bd55ee09239796f07d2dc
iamaniket58 <iamaniket58@gmail.com>
2025-07-06 02:11:06 +0530

    Made a Live API call E-6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,23 +1,35 @@
 import RestaurantCard from "./RestaurantCard"
+import { useEffect, useState } from "react";
 import restaurants from "../utils/mockRestroData";
-import { useState } from "react";
-import restaurants from "../utils/mockRestroData";
+import Shimmer from "./Shimmer"
 const Body = () => {
+   let[ListOfRestro,setListOfRestro]=useState([])
 
-   let[ListOfRestro,setListOfRestro]=useState(restaurants)
-
-    
+    useEffect(()=>{fetchData()},[])
+    const fetchData=async ()=>{
+        let data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9352403&lng=77.624532&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+        const responseJSON=await data.json();
+        setListOfRestro(responseJSON?.data?.cards[1]?.card.card.gridElements.infoWithStyle.restaurants)
+        console.log(responseJSON);
+        console.log("Another Restaurant")
+        console.log(responseJSON?.data?.cards[1]?.card.card.gridElements.infoWithStyle.restaurants)
+    }
+    if(ListOfRestro.length===0){
+        return(
+            <Shimmer/>
+        )
+    }
     return (
+        
         <div className="body">
             <div className="filter">
                 <button className="filter-btn" onClick={() => {
-                    console.log("Button is clicked");
                     const filtered = ListOfRestro.filter(x => x.info.avgRating >= 4.7);
                     setListOfRestro(filtered)
-                    console.log(setListOfRestro);
                 }}
                 >Top Rated Restaurants</button>
             </div>
+             {console.log('return Called')}
             <div className="res-container">
                 {/* <RestaurantCard resData={restaurants[0]} />
                 <RestaurantCard resData={restaurants[1]} />
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,7 +1,7 @@
 import { Component } from "react";
 import {CDN_URL} from "../utils/constant"
 const RestaurantCard = (props) => {
-    { console.log(props) }
+    //{ console.log(props) }
     let{name,cuisines,avgRating,sla,costForTwo, cloudinaryImageId}=props?.resData?.info
     return (
 
