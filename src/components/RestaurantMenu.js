085ee1d3f83413107329bbde7b0b4e1362666c09
iamaniket58 <iamaniket58@gmail.com>
2025-07-21 01:50:00 +0530

    Fixed APi calls- handled resInfo in 1 State Variable

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,41 +1,52 @@
-import { useEffect, useState, } from "react"
-import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
 import { MENU_API } from "../utils/constant";
+
 const RestaurantMenu = () => {
-    const [resInfo, setResInfo] = useState([]);
-    const [menuItems, setmenuItems] = useState([]);
+    const [resInfo, setResInfo] = useState(null);
+
     const { resId } = useParams();
-    useEffect(() => { fetchMenu() }, [])
+
+    useEffect(() => {
+        fetchMenu();
+    }, []);
+
     const fetchMenu = async () => {
-        const data = await fetch(MENU_API + resId + "&catalog_qa=undefined&submitAction=ENTER")
-        const res = await data.json();
-        console.log('Response is:', res.data)
-        console.log(res?.data?.cards[2]?.card?.card?.info)
-        let info = res?.data?.cards[2]?.card?.card?.info
-        setResInfo(info)
-        let menu = res?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards //|| res?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[6]?.card?.card?.itemCards
-        setmenuItems(menu)
-        console.log(menu)
-    }
-    const { name, cuisines, totalRatingsString, cloudinaryImageId, costForTwoMessage } = resInfo
-    return resInfo.length == 0 ? (<Shimmer />) : (
+        const data = await fetch(MENU_API + resId);
+        const json = await data.json();
+        setResInfo(json.data);
+    };
+
+    if (resInfo === null) return <Shimmer />;
+
+    const { name, cuisines, costForTwoMessage } =
+        resInfo?.cards[2]?.card?.card?.info;
+    console.log('Res cards')
+    console.log(resInfo);
+    const { itemCards } =
+        resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card 
+
+    console.log('Item cards')
+    console.log(itemCards);
+
+    return (
         <div className="menu">
             <h1>{name}</h1>
-            <h3>{cuisines.join(",")}- {costForTwoMessage}</h3>
-            <h3>{totalRatingsString}</h3>
-
-            <h4>Menu</h4>
+            <p>
+                {cuisines.join(", ")} - {costForTwoMessage}
+            </p>
+            <h2>Menu</h2>
             <ul>
-                {menuItems.map(item =>
+                {itemCards.map((item) => (
                     <li key={item.card.info.id}>
-                        {item.card.info.name} -Rs  {(item?.card?.info?.price) / 100 || (item?.card?.info?.defaultPrice) / 100}
-                    </li>)}
-                {/* <li>{menuItems[0]?.card?.info?.name}</li>
-                <li>{menuItems[1]?.card?.info?.name}</li>
-                <li>{menuItems[2]?.card?.info?.name}</li> */}
+                        {item.card.info.name} -{" Rs."}
+                        {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
+                    </li>
+                ))}
             </ul>
         </div>
-    )
-}
-export default RestaurantMenu
+    );
+};
+
+export default RestaurantMenu;
