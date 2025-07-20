603fa5b6243d802be28167f13e31bfafae16fef4
iamaniket58 <iamaniket58@gmail.com>
2025-07-20 20:26:04 +0530

    Created Class Based Component and introduced State Variables inside it

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
+import User from "./User"
+import UserClass from "./UserClass"
 const About = () => {
     return (
         <div>
             <h1>About</h1>
             <p>lorem22</p>
+            <User name={"Awesome Aniket"} T="They way"/>
+            <UserClass name={"Awesome Aniket class bases Comp"} location="Bengaluru Class" contact="aniket58"/>
         </div>
     )
 }
