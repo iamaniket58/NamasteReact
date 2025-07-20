abc5a92e43f3fa9831981448f91ebeaefd5eb204
iamaniket58 <iamaniket58@gmail.com>
2025-07-20 23:38:11 +0530

    Github Users APi call and data updation

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,33 +3,35 @@ class UserClass extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            // count: 0,
-            // count2: 22
+            userInfo: {
+                name: "Dummy",
+                location: "Default",
+                login: "null",
+                avatar_url:"null"
+            }
         }
-        // console.log(this.props.name+'Child Constructor ')
 
     }
-    componentDidMount() {
-        // console.log(this.props.name+'Child Component Did Mount')
-
+    async componentDidMount() {
+        const data = await fetch('https://api.github.com/users/iamaniket58')
+        const res = await data.json();
+        this.setState({
+            userInfo: res
+        })
+        console.log(res)
+    }
+    componentDidUpdate(){
+        console.log('✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅✅')
     }
     render() {
-        // console.log(this.props.name+'Child Render')
-        // console.log(this.props?.name)
-        const { name, location, contact } = this.props
+         const { name, location, login, id,avatar_url } = this.state.userInfo
         return (
             <div className="user-card">
-                {/* <h2>{this.state.count}</h2>
-                <h2>{this.state.count2}</h2>
-                <button onClick={() => {
-                    this.setState({
-                        count: this.state.count + 1,
-                        count2: this.state.count2 * 2
-                    })
-                }}>Count Increment</button> */}
+                <h2>{id}</h2>
                 <h2>Name: {name}</h2>
                 <h3>Location: {location}</h3>
-                <h3>Contact:  {contact}</h3>
+                <h3>Contact:  {login}</h3>
+                <img src={avatar_url} alt="User Pic" ></img>
             </div>
         )
     }
