3d660660e67d26a07ba8b188c12fbd507546c45b
iamaniket58 <iamaniket58@gmail.com>
2025-08-12 02:07:06 +0530

    context provider

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{lazy, Suspense} from "react";
+import React,{lazy, Suspense, useEffect,useState} from "react";
 import ReactDOM from "react-dom/client";
 import Body from "./components/Body";
 import Header from "./components/Header";
@@ -7,16 +7,28 @@ import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
+// import UserContext from "./utils/UserContext";
+import UserContext from "./utils/UserContext";
 //import Grocery from "./components/Grocery";
 
 const Grocery=lazy(()=>import("./components/Grocery"))
 const About=lazy(()=>import("./components/About"))
 const AppLayout = () => {
+    const[userName,setUserName]=useState('Kir');
+
+    useEffect(()=>{
+        const data={
+            name:'Aniket Kumar Choudhary'
+        }
+        setUserName(data.name)
+    },[])
     return (
+        <UserContext.Provider value={{loggedInUser:userName}}>
         <div className="app">
             <Header />
             <Outlet/>
         </div>
+        </UserContext.Provider>
     )
 }
 const appRouter = createBrowserRouter([
